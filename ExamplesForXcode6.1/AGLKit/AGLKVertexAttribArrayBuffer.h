6d4c97badff89b605360423fab011f28a861b493
xuhf <xuhf@bairuitech.com>
2017-05-02 18:24:03 +0800

    添加

---
+++ ExamplesForXcode6.1/AGLKit/AGLKVertexAttribArrayBuffer.h
@@ -0,0 +1,57 @@
+//
+//  AGLKVertexAttribArrayBuffer.h
+//  
+//
+
+#import <GLKit/GLKit.h>
+
+@class AGLKElementIndexArrayBuffer;
+
+/////////////////////////////////////////////////////////////////
+// 
+typedef enum {
+    AGLKVertexAttribPosition = GLKVertexAttribPosition,
+    AGLKVertexAttribNormal = GLKVertexAttribNormal,
+    AGLKVertexAttribColor = GLKVertexAttribColor,
+    AGLKVertexAttribTexCoord0 = GLKVertexAttribTexCoord0,
+    AGLKVertexAttribTexCoord1 = GLKVertexAttribTexCoord1,
+} AGLKVertexAttrib;
+
+
+@interface AGLKVertexAttribArrayBuffer : NSObject
+{
+   GLsizei      stride;
+   GLsizeiptr   bufferSizeBytes;
+   GLuint       name;
+}
+
+@property (nonatomic, readonly) GLuint
+   name;
+@property (nonatomic, readonly) GLsizeiptr
+   bufferSizeBytes;
+@property (nonatomic, readonly) GLsizei
+   stride;
+
++ (void)drawPreparedArraysWithMode:(GLenum)mode
+   startVertexIndex:(GLint)first
+   numberOfVertices:(GLsizei)count;
+
+- (id)initWithAttribStride:(GLsizei)stride
+   numberOfVertices:(GLsizei)count
+   bytes:(const GLvoid *)dataPtr
+   usage:(GLenum)usage;
+
+- (void)prepareToDrawWithAttrib:(GLuint)index
+   numberOfCoordinates:(GLint)count
+   attribOffset:(GLsizeiptr)offset
+   shouldEnable:(BOOL)shouldEnable;
+
+- (void)drawArrayWithMode:(GLenum)mode
+   startVertexIndex:(GLint)first
+   numberOfVertices:(GLsizei)count;
+   
+- (void)reinitWithAttribStride:(GLsizei)stride
+   numberOfVertices:(GLsizei)count
+   bytes:(const GLvoid *)dataPtr;
+   
+@end
